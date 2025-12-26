cd9f3e9af1e55e36dbcd8717bdf64f32ca66bccd
bala-amypo <bala@amypo.in>
2025-12-26 08:56:59 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UrgencyPolicyServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UrgencyPolicyServiceImpl.java
@@ -3,28 +3,29 @@ package com.example.demo.service.impl;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.UrgencyPolicy;
 import com.example.demo.repository.UrgencyPolicyRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class UrgencyPolicyServiceImpl {
 
-    private final UrgencyPolicyRepository policyRepository;
+    private final UrgencyPolicyRepository urgencyPolicyRepository;
 
-    public UrgencyPolicyServiceImpl(UrgencyPolicyRepository policyRepository) {
-        this.policyRepository = policyRepository;
+    public UrgencyPolicyServiceImpl(UrgencyPolicyRepository urgencyPolicyRepository) {
+        this.urgencyPolicyRepository = urgencyPolicyRepository;
     }
 
     public UrgencyPolicy createPolicy(UrgencyPolicy policy) {
-        return policyRepository.save(policy);
+        return urgencyPolicyRepository.save(policy);
     }
 
     public UrgencyPolicy getPolicy(Long id) {
-        return policyRepository.findById(id)
-                .orElseThrow(() ->
-                        new ResourceNotFoundException("Policy not found"));
+        return urgencyPolicyRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Urgency policy not found"));
     }
 
     public List<UrgencyPolicy> getAllPolicies() {
-        return policyRepository.findAll();
+        return urgencyPolicyRepository.findAll();
     }
 }
