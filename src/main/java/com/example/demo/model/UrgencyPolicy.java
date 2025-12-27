08db0f15ab140d4e66334baf4e4110b4a159df3e
bala-amypo <bala@amypo.in>
2025-12-27 05:23:59 +0000

    User commit

--- src/main/java/com/example/demo/model/UrgencyPolicy.java
+++ src/main/java/com/example/demo/model/UrgencyPolicy.java
@@ -1,10 +1,36 @@
 package com.example.demo.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class UrgencyPolicy {
 
-    private String urgency;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    public String getUrgency() {
-        return urgency;
+    private String keyword;
+
+    private String urgencyLevel;
+
+    // getters & setters
+    public Long getId() {
+        return id;
+    }
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+
+    public String getUrgencyLevel() {
+        return urgencyLevel;
+    }
+
+    public void setUrgencyLevel(String urgencyLevel) {
+        this.urgencyLevel = urgencyLevel;
     }
 }
